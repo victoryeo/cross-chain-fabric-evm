66e0e631fc02fd265afc08d39fcaa2402b14ce0e
yeosiowvic <victoryeo1974@gmail.com>
2023-04-16 10:12:43 +0800

    add chaincode skeleton Invoke code

--- chaincode/src/chaincode.ts
+++ chaincode/src/chaincode.ts
@@ -7,8 +7,28 @@ export default class TokenChaincode {
    * @returns
    * @memberof TokenChaincode
    */
-   public async Init() {
+  public async Init() {
     return Shim.success();
   }
 
+  /**
+   * Invoke function, it will be invoked by Chaincode client
+   *
+   * @param {ChaincodeStub} chaincodeStub
+   * @returns
+   * @memberof TokenChaincode
+   */
+  public async Invoke(chaincodeStub: ChaincodeStub) {
+    try {
+      // Client identity information when it is registered to CA
+      const clientIdentity = this.createClientIdentity(chaincodeStub);
+    } catch (error) {
+      console.log(`Error: ${error}`);
+      return Shim.error(error);
+    }
+  }
+
+  private createClientIdentity(chaincodeStub: ChaincodeStub): ClientIdentity {
+    return new ClientIdentity(chaincodeStub);
+  }
 }
