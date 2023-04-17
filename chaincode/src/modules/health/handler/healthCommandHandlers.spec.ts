bc828153cdcca91e3ff08e727e0c4224685d6436
yeosiowvic <victoryeo1974@gmail.com>
2023-04-17 13:06:32 +0800

    enhance test code

--- chaincode/src/modules/health/handler/healthCommandHandlers.spec.ts
+++ chaincode/src/modules/health/handler/healthCommandHandlers.spec.ts
@@ -1,25 +1,32 @@
 import { ChaincodeMockStub } from '@theledger/fabric-mock-stub';
 import { expect } from 'chai';
 import 'mocha';
-import Chaincode from '../../../chaincode';
+import TokenChaincode from '../../../chaincode';
 
-import valid_CmdFixture from '../../../../test/fixtures/health/validCommand.json';
+import valid_CmdFixture from '../../../test/fixtures/health/valid_cmd.json';
 
-const chaincode = new Chaincode();
-describe('Commands', () => {
+const chaincode = new TokenChaincode();
 
-  it('Should able to be healthy', async () => {
+describe('Test Health', () => {
+  it('Should initailize', async () => {
+    const mockStub = new ChaincodeMockStub('MyMockStub', chaincode);
+    const response = await mockStub.mockInit('tx1', []);
+    expect(response.status).to.equal(200);
+  });
+
+  it('Should be healthy', async () => {
     const mockStub = new ChaincodeMockStub('TestMockStub', chaincode);
     await mockStub.mockInit('tx1', []);
     const txID = 'TX0001';
     mockStub.mockTransactionStart(txID);
 
+    console.log('fixture name: ', valid_CmdFixture.name)
     const response = await mockStub.mockInvoke(txID, [
       valid_CmdFixture.name
     ]);
     mockStub.mockTransactionEnd(txID);
     expect(response.status).to.equal(200);
-    expect(JSON.parse(response.payload.toString())).to.equal('healthy');
+    expect(JSON.parse(response?.payload?.toString())).to.equal('healthy');
   });
 
 });
