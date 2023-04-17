ca13968e209d8824c5b46839fe213939dd19fdf2
yeosiowvic <victoryeo1974@gmail.com>
2023-04-17 11:17:50 +0800

    add BaseCommandHandler interface

--- chaincode/src/common/index.ts
+++ chaincode/src/common/index.ts
@@ -1,3 +1,5 @@
+import { ChaincodeStub } from 'fabric-shim';
+
 export interface UserIdentity {
   id: string;
   mspId: string;
@@ -6,4 +8,14 @@ export interface UserIdentity {
 export interface BaseCommand {
   name: string;
   payload: any;
+}
+
+export interface BaseCommandHandler {
+  isMatch(command: BaseCommand): boolean;
+
+  handleCommand(
+    command: BaseCommand,
+    chaincodeStub: ChaincodeStub,
+    userIdentity: UserIdentity
+  ): Promise<any>;
 }
