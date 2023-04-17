4d2550cbb0fc1b499a5059f173269e0535b1b762
yeosiowvic <victoryeo1974@gmail.com>
2023-04-17 11:17:37 +0800

    add command handler processing

--- chaincode/src/command/index.ts
+++ chaincode/src/command/index.ts
@@ -1,5 +1,12 @@
 import { ChaincodeStub } from 'fabric-shim';
-import { UserIdentity, BaseCommand } from '../common';
+import { UserIdentity, BaseCommand, BaseCommandHandler } from '../common';
+import { HealthCommandHandler } from '../modules/health';
+/**
+ * Define all command handler here
+ */
+ const commandHandlers: BaseCommandHandler[] = [
+  new HealthCommandHandler(),
+];
 
 /**
  * It will find command handler to process the command
@@ -9,10 +16,28 @@ import { UserIdentity, BaseCommand } from '../common';
  * @param {UserIdentity} userIdentity
  * @returns
  */
- export const handleCommand = async (
+export const findHandleCommand = async (
   chaincodeStub: ChaincodeStub,
   command: BaseCommand,
   userIdentity: UserIdentity
 ) => {
-  return 0;
+  // Find handler by command
+  const commandHandler = commandHandlers.find(handler => {
+    return handler.isMatch(command);
+  });
+  // Handler is not found
+  if (!commandHandler) {
+    console.error("Handler is not found")
+  }
+
+  try {
+    return await commandHandler.handleCommand(
+      command,
+      chaincodeStub,
+      userIdentity
+    );
+  } catch (e) {
+    console.log(`Error: ${e}`);
+    throw e;
+  }
 }
