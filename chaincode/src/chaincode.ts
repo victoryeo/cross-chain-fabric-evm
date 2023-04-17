f926c081a2481498fd052c33f57f589aefadb526
yeosiowvic <victoryeo1974@gmail.com>
2023-04-17 10:42:01 +0800

    call client identity and command handler
    Signed-off-by: yeosiowvic <victoryeo1974@gmail.com>

--- chaincode/src/chaincode.ts
+++ chaincode/src/chaincode.ts
@@ -1,4 +1,6 @@
 import { ClientIdentity, ChaincodeStub, Shim } from 'fabric-shim';
+import { UserIdentity } from './common';
+import { handleCommand } from './command';
 
 export default class TokenChaincode {
   /**
@@ -22,6 +24,30 @@ export default class TokenChaincode {
     try {
       // Client identity information when it is registered to CA
       const clientIdentity = this.createClientIdentity(chaincodeStub);
+
+      const identity: UserIdentity = {
+        id: clientIdentity.getID(),
+        mspId: clientIdentity.getMSPID(),
+      };
+
+      if (!identity.mspId) {
+        console.error("Bad Client identity");
+      }
+      console.info(JSON.stringify(identity));
+      const ret = chaincodeStub.getFunctionAndParameters();
+      console.info(ret.fcn);
+
+      const command = {
+        name: ret.fcn, // Function name, will be mapped to command name
+        payload: JSON.parse(ret.params[0] ?? null), 
+        // Only accept first param as JSON object
+      };
+
+      // Handle command
+      const result = await handleCommand(chaincodeStub, command, identity);
+
+      // Return result
+      return Shim.success(Buffer.from(JSON.stringify(result)));
     } catch (error) {
       console.log(`Error: ${error}`);
       return Shim.error(error);
