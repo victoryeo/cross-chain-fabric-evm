3eafdf1fd362bb0862fea3b745fdd4849e0bed18
yeosiowvic <victoryeo1974@gmail.com>
2023-04-22 12:29:14 +0800

    add function to get status from chaincode

--- app/src/modules/cchain/cchain.module.ts
+++ app/src/modules/cchain/cchain.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CChainController } from './cchain.controller';
 import { CChainService } from './cchain.service';
+import { BlockchainUtil } from '../../common/utils';
 
 @Module({
   controllers: [CChainController],
-  providers: [CChainService],
+  providers: [CChainService, BlockchainUtil],
 })
 
 export class CChainModule {}
--- app/src/modules/cchain/cchain.service.ts
+++ app/src/modules/cchain/cchain.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@nestjs/common';
-
+import { BlockchainUtil } from '../../common/utils';
 
 @Injectable()
 export class CChainService {
+  constructor(private blockchainUtil: BlockchainUtil) { }
+
+  async getStatus(): Promise<string> {
+    const ccPath: string = `${process.env.BC_HLF_URL}/api/fabric-client/cc/health/chaincode/GetHealth`;
+    const params = {};
+
+    const retData = await this.blockchainUtil.sendGetRequestToHLF(
+      ccPath,
+      params
+    );
+    const resDTO = JSON.parse(retData.data.result);
+
+    return resDTO;
+  }
+
   async ImportToken(): Promise<any> {
     return 0;
   }
