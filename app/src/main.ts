1d0eedc389e2a88f6d3692d9fee449c865ebd21a
yeosiowvic <victoryeo1974@gmail.com>
2023-10-20 12:37:29 +0800

    use port number

--- app/src/main.ts
+++ app/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  console.log(`PORT ${process.env.PORT_NO}`)
+  await app.listen(process.env.PORT_NO || 3000);
 }
 bootstrap();
